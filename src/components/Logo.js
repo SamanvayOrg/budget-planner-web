b0bb63582a116e56ef129aa663059797b7aeee9d
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-05-17 14:01:05 +0530

    Fix warnings

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,6 +3,6 @@ import "../style.css";
 
 
 const Logo = () => {
-	return <img src={mbs} className="navBarLogo" />;
+	return <img src={mbs} className="navBarLogo"  alt="logo"/>;
 };
 export default Logo;
--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import {TextField} from "@mui/material";
-import {Label} from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 
 const LogoutButton = () => {
