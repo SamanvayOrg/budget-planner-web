83ae5e6c8ce83612981570a6ea40332558a44823
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-05-25 19:03:37 +0530

    Show empty budget box on dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,6 +1,55 @@
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
+import {makeStyles} from "@mui/styles";
+import Container from "@mui/material/Container";
+import UserName from "../components/UserName";
+import EmptyBudgetBox from "../components/EmptyBudgetBox";
+
+const useStyles = makeStyles(theme => ({
+	mainContainer: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "start",
+		paddingTop: "6%",
+		paddingLeft: "1%",
+		fontFamily: "Lato",
+		fontStyle: "normal",
+		color: "#616161",
+		fontWeight: "700",
+
+	}, leftUserNameText: {
+		display: "flex",
+		flexDirection: "row",
+		fontSize: "11px",
+		letterSpacing: "0.06em",
+		textTransform: "uppercase",
+		marginBottom: "1%"
+	}, welcomeText: {
+		fontWeight: "400", fontSize: "21px", lineHeight: "25px",
+	}, mmbsName: {
+		fontStyle: "italic", fontSize: "21px", lineHeight: "25px", color: "black",
+
+	}
+
+}))
 
 const Dashboard = () => {
-	return <ResponsiveAppBar/>
+	const classes = useStyles();
+	return (
+		<div>
+			<ResponsiveAppBar/>
+			<Container maxWidth="xl">
+				<div className={classes.mainContainer}>
+					<div className={classes.leftUserNameText}>
+						HELLO <UserName/>
+					</div>
+					<div><span className={classes.welcomeText}>Welcome to </span>
+						<span className={classes.mmbsName}>Maharashtra Municipality budgeting system.</span>
+					</div>
+					<EmptyBudgetBox/>
+				</div>
+			</Container>
+		</div>
+	)
 };
 export default Dashboard;
