eafe9d4ee2432b33f4b5f5ac06acaec3cf309c47
Vinay Venu <ijkvinay@gmail.com>
2022-06-07 12:09:38 +0530

    Change api to point to new urls

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 const getCurrentBudget = async (token) => {
 	const headers = {'Authorization': `Bearer ${token}`};
-	const result = await axios.get("/budget/current",
+	const result = await axios.get("/api/budget/current",
 		{
 			headers
 		});
