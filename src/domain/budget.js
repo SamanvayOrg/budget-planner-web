3f098ec25414d45a7d9c77b3a498b19411b73929
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-06-14 16:25:46 +0530

    Map contract to domain object

--- src/domain/budget.js
+++ src/domain/budget.js
@@ -1,38 +1,54 @@
-// let budget = {};
-//
-// const setBudget = (budgetObject) => {
-//     budget = budgetObject;
-// };
-//
-// const getLinesToDisplay = (budget) => {
-//     const groupedItems = budget.budgetLines.groupBy(({majorHeadGroup}) => majorHeadGroup)
-// }
-
-
 import _ from 'lodash';
 
-const linesForMajorHeadGroup = (lines, majorHeadGroup) => {
-    _.filter(lines, line => line.majorHeadGroup === majorHeadGroup);
-};
-
 const fromContract = ({budgetYear, budgetLines}) => {
-    const orderedMajorHeadGroups = _.chain(budgetLines)
-        .map(({majorHeadGroup, majorHeadGroupDisplayOrder}) => ({majorHeadGroup, majorHeadGroupDisplayOrder}))
-        .uniqBy('majorHeadGroup')
-        .sortBy(['majorHeadGroupDisplayOrder'])
-        .map(({majorHeadGroup}) => majorHeadGroup)
-        .value();
-    const majorHeadGroupItems = _.map(orderedMajorHeadGroups, majorHeadGroup => ({
-        majorHeadGroup,
-        items: linesForMajorHeadGroup(budgetLines, majorHeadGroup)
-    }));
+	const filterByKeyValue = (budgetLines, key, value) => {
+		return _.filter(budgetLines, line => line[key] === value);
+	}
 
-    return {
-        year: Number.parseInt(budgetYear.substring(0, 4)),
-        items: majorHeadGroupItems
-    };
+	const mapMajorHeadGroups = (allLines) => {
+		const orderedMajorHeadGroups = _.chain(allLines)
+			.map(({majorHeadGroup, majorHeadGroupDisplayOrder}) => ({majorHeadGroup, majorHeadGroupDisplayOrder}))
+			.uniqBy('majorHeadGroup')
+			.sortBy(['majorHeadGroupDisplayOrder'])
+			.map(({majorHeadGroup}) => majorHeadGroup)
+			.value();
+
+		return _.map(orderedMajorHeadGroups, majorHeadGroup =>
+			mapMajorHeadGroup(filterByKeyValue(allLines, 'majorHeadGroup', majorHeadGroup), majorHeadGroup));
+	}
+
+	const mapMajorHeadGroup = (linesInMajorHeadGroup, majorHeadGroup) => ({
+		majorHeadGroup,
+		items: mapMajorHeads(linesInMajorHeadGroup),
+	});
+
+	const mapMajorHeads = (linesInMajorHeadGroup) => {
+		const majorHeads = _.chain(linesInMajorHeadGroup).map(({majorHead}) => majorHead).uniq().value();
+		return _.map(majorHeads, majorHead => mapMajorHead(filterByKeyValue(linesInMajorHeadGroup, 'majorHead', majorHead), majorHead))
+	};
+
+	const mapMajorHead = (linesInMajorHead, majorHead) => {
+		return {
+			majorHead,
+			items: mapLineItem(linesInMajorHead)
+		}
+	}
+
+	const mapLineItem = (linesInMajorHead) => {
+		return _.map(linesInMajorHead, line => (
+			{
+				...line,
+				name: line.minorHead + ' - ' + line.name,
+			}
+		));
+	}
+
+	return {
+		year: Number.parseInt(budgetYear.substring(0, 4)),
+		items: mapMajorHeadGroups(budgetLines)
+	};
 };
 
 export {
-    fromContract
+	fromContract
 };
--- src/domain/budget.test.js
+++ src/domain/budget.test.js
@@ -1,156 +1,192 @@
 import {fromContract} from './budget';
+import _ from "lodash";
 
 describe('budget', () => {
-    const budgetContract = {
-        "budgetYear": "2023-24",
-        "budgetLines": [{
-            "code": "910-1110",
-            "name": "All",
-            "budgetedAmount": null,
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 1.00,
-            "majorHeadGroup": "Revenue Receipt",
-            "majorHeadGroupDisplayOrder": 1.00,
-            "majorHead": "Property Tax",
-            "minorHead": "Consolidated Tax on Property"
-        }, {
-            "code": "910-1111",
-            "name": "Residential Property",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 2.00,
-            "majorHeadGroup": "Revenue Receipt",
-            "majorHeadGroupDisplayOrder": 1.00,
-            "majorHead": "Property Tax",
-            "minorHead": "Consolidated Tax on Property"
-        }, {
-            "code": "920-1120",
-            "name": "All",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 3.00,
-            "majorHeadGroup": "Revenue Receipt",
-            "majorHeadGroupDisplayOrder": 1.00,
-            "majorHead": "Property Tax",
-            "minorHead": "Advertisement Tax"
-        }, {
-            "code": "920-1121",
-            "name": "Hoarding on Public Lands",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 4.00,
-            "majorHeadGroup": "Revenue Receipt",
-            "majorHeadGroupDisplayOrder": 1.00,
-            "majorHead": "Property Tax",
-            "minorHead": "Advertisement Tax"
-        }, {
-            "code": "310-2510",
-            "name": "All",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 5.00,
-            "majorHeadGroup": "Expenses",
-            "majorHeadGroupDisplayOrder": 2.00,
-            "majorHead": "Purchases for Operations and Programme implements tion",
-            "minorHead": "Purchase of Water for Supply"
-        }, {
-            "code": "314-2520",
-            "name": "All",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 6.00,
-            "majorHeadGroup": "Expenses",
-            "majorHeadGroupDisplayOrder": 2.00,
-            "majorHead": "Purchases for Operations and Programme implementation",
-            "minorHead": "Purchase of Consumables"
-        }, {
-            "code": "210-3221",
-            "name": "Road Grants",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 7.00,
-            "majorHeadGroup": "Liability",
-            "majorHeadGroupDisplayOrder": 3.00,
-            "majorHead": "Grants, Contributions for Specific Purpose (Earmarked Funds)",
-            "minorHead": "Government of Maharashtra (Urban Develonment Department)"
-        }, {
-            "code": "315-4234",
-            "name": "Roads & Foot Paths \\Vater Supply System",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 8.00,
-            "majorHeadGroup": "Assets",
-            "majorHeadGroupDisplayOrder": 4.00,
-            "majorHead": "Accumulated Depreciation",
-            "minorHead": "Other Fixed Assets"
-        }, {
-            "code": "740-3230",
-            "name": "All",
-            "currentYear8MonthsActuals": null,
-            "currentYear4MonthsProbables": null,
-            "previousYearActuals": null,
-            "yearMinus1Actuals": 100,
-            "yearMinus2Actuals": 200,
-            "yearMinus3Actuals": 200,
-            "displayOrder": 9.00,
-            "majorHeadGroup": "Liability",
-            "majorHeadGroupDisplayOrder": 3.00,
-            "majorHead": "Grants, Contributions for Specific Purpose (Earmarked Funds)",
-            "minorHead": "Government of Maharashtra (Other Diaa(tagnsive mats)"
-        }]
-    };
+	const budgetContract = {
+		"budgetYear": "2023-24",
+		"budgetLines": [{
+			"code": "910-1110",
+			"name": "All",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 1.00,
+			"majorHeadGroup": "Revenue Receipt",
+			"majorHeadGroupDisplayOrder": 1.00,
+			"majorHead": "Property Tax",
+			"minorHead": "Consolidated Tax on Property"
+		}, {
+			"code": "910-1111",
+			"name": "Residential Property",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 2.00,
+			"majorHeadGroup": "Revenue Receipt",
+			"majorHeadGroupDisplayOrder": 1.00,
+			"majorHead": "Property Tax",
+			"minorHead": "Consolidated Tax on Property"
+		}, {
+			"code": "920-1120",
+			"name": "All",
+			"budgetedAmount": 10,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 3.00,
+			"majorHeadGroup": "Revenue Receipt",
+			"majorHeadGroupDisplayOrder": 1.00,
+			"majorHead": "Property Tax",
+			"minorHead": "Advertisement Tax"
+		}, {
+			"code": "920-1121",
+			"name": "Hoarding on Public Lands",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 4.00,
+			"majorHeadGroup": "Revenue Receipt",
+			"majorHeadGroupDisplayOrder": 1.00,
+			"majorHead": "Property Tax",
+			"minorHead": "Advertisement Tax"
+		}, {
+			"code": "310-2510",
+			"name": "All",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 5.00,
+			"majorHeadGroup": "Expenses",
+			"majorHeadGroupDisplayOrder": 2.00,
+			"majorHead": "Purchases for Operations and Programme implementation",
+			"minorHead": "Purchase of Water for Supply"
+		}, {
+			"code": "314-2520",
+			"name": "All",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 6.00,
+			"majorHeadGroup": "Expenses",
+			"majorHeadGroupDisplayOrder": 2.00,
+			"majorHead": "Purchases for Operations and Programme implementation",
+			"minorHead": "Purchase of Consumables"
+		}, {
+			"code": "210-3221",
+			"name": "Road Grants",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 7.00,
+			"majorHeadGroup": "Liability",
+			"majorHeadGroupDisplayOrder": 3.00,
+			"majorHead": "Grants, Contributions for Specific Purpose (Earmarked Funds)",
+			"minorHead": "Government of Maharashtra (Urban Develonment Department)"
+		}, {
+			"code": "315-4234",
+			"name": "Roads & Foot Paths \\Vater Supply System",
+			"budgetedAmount": null,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 8.00,
+			"majorHeadGroup": "Assets",
+			"majorHeadGroupDisplayOrder": 4.00,
+			"majorHead": "Accumulated Depreciation",
+			"minorHead": "Other Fixed Assets"
+		}, {
+			"code": "740-3230",
+			"name": "All",
+			"budgetedAmount": 200,
+			"currentYear8MonthsActuals": null,
+			"currentYear4MonthsProbables": null,
+			"previousYearActuals": null,
+			"yearMinus1Actuals": 100,
+			"yearMinus2Actuals": 200,
+			"yearMinus3Actuals": 200,
+			"displayOrder": 9.00,
+			"majorHeadGroup": "Liability",
+			"majorHeadGroupDisplayOrder": 3.00,
+			"majorHead": "Grants, Contributions for Specific Purpose (Earmarked Funds)",
+			"minorHead": "Government of Maharashtra (Other Diaa(tagnsive mats)"
+		}]
+	};
 
-    it('should map year', () => {
-        let budget = fromContract(budgetContract);
-        expect(budget.year).toBe(2023);
-    });
+	it('should map year', () => {
+		let budget = fromContract(budgetContract);
+		expect(budget.year).toBe(2023);
+		console.log("log-->", budget.items[3]);
+	});
 
-    it('should map major head groups in the right order', () => {
-        let budget = fromContract(budgetContract);
-        expect(budget.majorHeadGroups[0].name).toBe('Revenue Receipt');
-        expect(budget.majorHeadGroups[1].name).toBe('Expenses');
-        expect(budget.majorHeadGroups[2].name).toBe('Liability');
-        expect(budget.majorHeadGroups[3].name).toBe('Assets');
-    });
+	it('should map major head groups in the right order', () => {
+		let budget = fromContract(budgetContract);
+		expect(budget.items[0].majorHeadGroup).toBe('Revenue Receipt');
+		expect(budget.items[1].majorHeadGroup).toBe('Expenses');
+		expect(budget.items[2].majorHeadGroup).toBe('Liability');
+		expect(budget.items[3].majorHeadGroup).toBe('Assets');
+	});
 
-    it('should map major heads in the right order', () => {
-        let budget = fromContract(budgetContract);
-        expect(budget.majorHeadGroups[0].name).toBe('Revenue Receipt');
-        expect(budget.majorHeadGroups[1].name).toBe('Expenses');
-        expect(budget.majorHeadGroups[2].name).toBe('Liability');
-        expect(budget.majorHeadGroups[3].name).toBe('Assets');
-    });
+	it('should map major heads in the right order', () => {
+		let budget = fromContract(budgetContract);
+		expect(budget.items[0].items[0].majorHead).toBe('Property Tax');
+		expect(budget.items[0].items[1].majorHead).toBe('Property Tax');
+		expect(budget.items[0].items[2].majorHead).toBe('Property Tax');
+		expect(budget.items[0].items[3].majorHead).toBe('Property Tax');
+		expect(budget.items[1].items[0].majorHead).toBe('Purchases for Operations and Programme implementation');
+		expect(budget.items[1].items[1].majorHead).toBe('Purchases for Operations and Programme implementation');
+		expect(budget.items[2].items[0].majorHead).toBe('Grants, Contributions for Specific Purpose (Earmarked Funds)');
+		expect(budget.items[2].items[1].majorHead).toBe('Grants, Contributions for Specific Purpose (Earmarked Funds)');
+		expect(budget.items[3].items[0].majorHead).toBe('Accumulated Depreciation');
+	});
+
+	it('should map minor heads in the right order', () => {
+		let budget = fromContract(budgetContract);
+		const revenueReceipt = _.find(budget.items, item => item.majorHeadGroup === 'Revenue Receipt');
+		expect(revenueReceipt).toBeDefined();
+		expect(revenueReceipt.items).toHaveLength(1);
+		const propertyTax = _.find(revenueReceipt.items, item => item.majorHead === 'Property Tax')
+		expect(propertyTax).toBeDefined();
+		expect(propertyTax.items).toHaveLength(4);
+		const allConsolidatedTaxOnProperty = _.find(propertyTax.items, item => item.name === 'Consolidated Tax on Property - All');
+		expect(allConsolidatedTaxOnProperty).toBeDefined();
+		expect(allConsolidatedTaxOnProperty.code).toBe('910-1110');
+		expect(allConsolidatedTaxOnProperty.budgetedAmount).toBeNull();
+		expect(allConsolidatedTaxOnProperty.currentYear8MonthsActuals).toBeNull()
+		expect(allConsolidatedTaxOnProperty.currentYear4MonthsProbables).toBeNull()
+		expect(allConsolidatedTaxOnProperty.previousYearActuals).toBeNull()
+		expect(allConsolidatedTaxOnProperty.yearMinus1Actuals).toBe(100);
+		expect(allConsolidatedTaxOnProperty.yearMinus2Actuals).toBe(200);
+		expect(allConsolidatedTaxOnProperty.displayOrder).toEqual(1);
+		console.log(budget)
+	});
 })
