e7d5892864e4a04c955ac4a424e7656b4ff76eef
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-07-14 12:00:37 +0530

    fix test

--- src/i18n/index.test.js
+++ src/i18n/index.test.js
@@ -1,20 +1,19 @@
 import {t} from "i18next";
-import i18next from "./index";
+import {i18n} from "./index";
 
 describe('translation', () => {
 
 	it('should get default translations', () => {
-		i18next.changeLanguage('mr');
+		i18n.changeLanguage('mr');
 
-		expect(t('Welcome to React')).toBe('Marathi translation');
+		expect(t('Welcome to React')).toBe('React मध्ये आपले स्वागत आहे');
 	});
 
 	it('should add new resources', () => {
-
-		i18next.addResources('mr', 'translation', {municipality: 'abc'});
-		i18next.changeLanguage('mr');
+		i18n.addResources('mr', 'translation', {municipality: 'abc'});
+		i18n.changeLanguage('mr');
 
 		expect(t('municipality')).toBe('abc');
-		expect(t('Welcome to React')).toBe('Marathi translation');
+		expect(t('Welcome to React')).toBe('React मध्ये आपले स्वागत आहे');
 	});
 });
