6cf8dfbf7b809bb518892d5ba804eadedb01c0c1
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-08-01 12:12:54 +0530

    remove population from municipality list

--- src/views/Municipality.js
+++ src/views/Municipality.js
@@ -12,23 +12,16 @@ import {useNavigate} from "react-router-dom";
 import Toolbar from "@mui/material/Toolbar";
 
 const Municipality = () => {
-    const [municipality, setMunicipality] = useState(null);
     const {details} = useSelector(allMunicipalityDetailsSelector)
-    const rowClick = (data) => {
-        console.log('clickedfs---', JSON.stringify(data), data.id);
-        setMunicipality(data);
-    }
-    const columns = [{id: 'name', label: 'Municipality name', minWidth: 170}, {
-        id: 'state', label: 'State', minWidth: 100
-    }, {id: 'population', label: 'Population', minWidth: 100}, {
-        id: 'cityClass', label: 'Municipality class', minWidth: 170,
-    }];
+
+    const columns = [{id: 'name', label: 'Municipality name', minWidth: 170},
+        {id: 'state', label: 'State', minWidth: 100},
+        {id: 'cityClass', label: 'Municipality class', minWidth: 170,}];
     let rows = [];
     if (!_.isEmpty(details)) {
         rows = [{
             "name": details.name,
-            "state": details.state.name,
-            "population": details.population,
+            "state": details.state,
             "cityClass": details.cityClass
         }]
     }
@@ -53,11 +46,9 @@ const Municipality = () => {
                 <HorizontalMenuDrawer menuList={adminMenus} drawerWidth={240} onClick={handleClick}/>
                 <Box component="main" sx={{flexGrow: 1, p: 3}}>
                     <Toolbar/>
-                <Paper sx={{width: '100%', overflow: 'hidden', paddingTop: "40px"}}>
-                    <ResponsiveTable columns={columns} rows={rows} onClick={(data) => {
-                        rowClick(data)
-                    }}/></Paper>
-            </Box>
+                    <Paper sx={{width: '100%', overflow: 'hidden', paddingTop: "40px"}}>
+                        <ResponsiveTable columns={columns} rows={rows} /></Paper>
+                </Box>
             </Box>);
     }
 
