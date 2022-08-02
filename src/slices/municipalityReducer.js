046fab7461f9ffc445c424e456e1aafee8e9d891
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-08-02 22:02:24 +0530

    fix error on refreshing municipality

--- src/slices/municipalityReducer.js
+++ src/slices/municipalityReducer.js
@@ -3,44 +3,44 @@ import {tokenSelector} from "./authReducer";
 import {getMunicipalityDetails} from "../api/api";
 
 export const initialState = {
-	details: {},
-	loading: false,
-	error: false
+    details: {},
+    loading: false,
+    error: false
 }
 const municipalityDetailsSlice = createSlice({
-	name: 'municipalityDetails',
-	initialState,
-	reducers: {
-		setDetails: (state, {payload}) => {
-			state.details = payload
-			state.loading = false
-			state.error = false
-		},
-		loading: (state) => {
-			state.loading = true
-			state.error = false
-		},
-		loadingFailure: (state) => {
-			state.loading = false
-			state.error = true
-		}
-	}
+    name: 'municipalityDetails',
+    initialState,
+    reducers: {
+        setDetails: (state, {payload}) => {
+            state.details = payload
+            state.loading = false
+            state.error = false
+        },
+        loading: (state) => {
+            state.loading = true
+            state.error = false
+        },
+        loadingFailure: (state) => {
+            state.loading = false
+            state.error = true
+        }
+    }
 })
 export const {
-	setDetails,
-	loading, loadingFailure
+    setDetails,
+    loading, loadingFailure
 } = municipalityDetailsSlice.actions;
 
 export const allMunicipalityDetailsSelector = state => state.municipalityDetails;
 export default municipalityDetailsSlice.reducer;
 
 export function fetchMunicipalityDetails() {
-	return async (dispatch, getState) => {
-		const token = tokenSelector(getState() || localStorage.getItem('authToken'));
-		dispatch(setDetails());
-		let details = await getMunicipalityDetails(token);
-		dispatch(setDetails(details));
-	}
+    return async (dispatch, getState) => {
+        const token = tokenSelector(getState()) || localStorage.getItem('authToken');
+        dispatch(setDetails());
+        let details = await getMunicipalityDetails(token);
+        dispatch(setDetails(details));
+    }
 }
 
 
--- src/views/Municipality.js
+++ src/views/Municipality.js
@@ -2,28 +2,38 @@ import {Box, Paper} from "@mui/material";
 import _ from "lodash";
 import ResponsiveTable from "../components/ResponsiveTable";
 import * as React from "react";
-import {useState} from "react";
-import {useSelector} from "react-redux";
-import {allMunicipalityDetailsSelector} from "../slices/municipalityReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {allMunicipalityDetailsSelector, fetchMunicipalityDetails} from "../slices/municipalityReducer";
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 import HorizontalMenuDrawer from "../components/HorizontalMenuDrawer";
 import {adminMenus} from "../config";
 import {useNavigate} from "react-router-dom";
 import Toolbar from "@mui/material/Toolbar";
+import {useEffect} from "react";
 
 const Municipality = () => {
-    const {details} = useSelector(allMunicipalityDetailsSelector)
+    const {details} = useSelector(allMunicipalityDetailsSelector);
+    const dispatch = useDispatch();
 
-    const columns = [{id: 'name', label: 'Municipality name', minWidth: 170},
+    useEffect(() => {
+        dispatch(fetchMunicipalityDetails());
+    }, [dispatch]);
+
+    const columns = [
+        {id: 'name', label: 'Municipality name', minWidth: 170},
         {id: 'state', label: 'State', minWidth: 100},
-        {id: 'cityClass', label: 'Municipality class', minWidth: 170,}];
+        {id: 'cityClass', label: 'Municipality class', minWidth: 170,}
+    ];
+
     let rows = [];
     if (!_.isEmpty(details)) {
-        rows = [{
-            "name": details.name,
-            "state": details.state,
-            "cityClass": details.cityClass
-        }]
+        rows = [
+            {
+                "name": details.name,
+                "state": details.state,
+                "cityClass": details.cityClass
+            }
+        ]
     }
     let navigate = useNavigate();
     const handleClick = (data) => {
@@ -47,7 +57,7 @@ const Municipality = () => {
                 <Box component="main" sx={{flexGrow: 1, p: 3}}>
                     <Toolbar/>
                     <Paper sx={{width: '100%', overflow: 'hidden', paddingTop: "40px"}}>
-                        <ResponsiveTable columns={columns} rows={rows} /></Paper>
+                        <ResponsiveTable columns={columns} rows={rows}/></Paper>
                 </Box>
             </Box>);
     }
