337ba769fc62bff327ef496bb97021982a96d529
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-08-11 13:36:45 +0530

    get population from budget in summary reports

--- src/components/CurrentBudgetBox.js
+++ src/components/CurrentBudgetBox.js
@@ -17,129 +17,129 @@ import PerPersonExpenditure from "./PerPersonExpenditure";
 import PerPersonRevenue from "./PerPersonRevenue";
 
 const styleSheets = makeStyles(theme => ({
-	box: {
-		display: "flex",
-		flexDirection: "row",
-		justifyContent: "space-between",
-		boxSizing: "border-box",
-		width: "100%",
-		background: " #FFFFFF",
-		border: "1px solid #DEDEDE",
-		borderRadius: "3px",
-		marginTop: "30px",
-		gap: "2vw",
-		paddingTop: "15px",
-		paddingBottom: "15px",
-		paddingRight: "10px",
-		fontSize: "15px",
-		fontWeight: "400",
-		fontFamily: "Lato",
-	}, innerBox: {
-		display: "flex",
-		justifyContent: "flex-start'",
-		gap: "0.5vw",
-		color: "#212121",
-		fontSize: "19px",
-		marginLeft: "1%",
-	}, text: {
-		fontWeight: "700", fontSize: "19px"
-	}, actionButtons: {
-		display: "flex", justifyContent: "flex-end", fontSize: "11px", textTransform: "uppercase", color: "#616161",
-	},
+    box: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        boxSizing: "border-box",
+        width: "100%",
+        background: " #FFFFFF",
+        border: "1px solid #DEDEDE",
+        borderRadius: "3px",
+        marginTop: "30px",
+        gap: "2vw",
+        paddingTop: "15px",
+        paddingBottom: "15px",
+        paddingRight: "10px",
+        fontSize: "15px",
+        fontWeight: "400",
+        fontFamily: "Lato",
+    }, innerBox: {
+        display: "flex",
+        justifyContent: "flex-start'",
+        gap: "0.5vw",
+        color: "#212121",
+        fontSize: "19px",
+        marginLeft: "1%",
+    }, text: {
+        fontWeight: "700", fontSize: "19px"
+    }, actionButtons: {
+        display: "flex", justifyContent: "flex-end", fontSize: "11px", textTransform: "uppercase", color: "#616161",
+    },
 
 }))
 const CurrentBudgetBox = ({year}) => {
-	const {allBudget} = useSelector(allBudgetSelector);
-	const dispatch = useDispatch();
-	const classes = styleSheets();
-	const [budgetYear, setBudgetYear] = useState(year);
-	let navigate = useNavigate();
-	const {currentMunicipality} = useSelector(allMunicipalityDetailsSelector);
+    const {allBudget} = useSelector(allBudgetSelector);
+    const dispatch = useDispatch();
+    const classes = styleSheets();
+    const [budgetYear, setBudgetYear] = useState(year);
+    let navigate = useNavigate();
+    const {currentMunicipality} = useSelector(allMunicipalityDetailsSelector);
 
-	useEffect(() => {
-		dispatch(fetchAllBudgets());
-		dispatch(fetchMunicipalityDetails());
-	}, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchAllBudgets());
+        dispatch(fetchMunicipalityDetails());
+    }, [dispatch]);
 
-	const getBudgetYears = (allBudgets) => {
-		let budgetYears = [];
-		_.forEach(allBudgets, budget => {
-			budgetYears.push(budget.budgetYear);
-		});
-		return _.sortBy(budgetYears).reverse();
-	};
+    const getBudgetYears = (allBudgets) => {
+        let budgetYears = [];
+        _.forEach(allBudgets, budget => {
+            budgetYears.push(budget.budgetYear);
+        });
+        return _.sortBy(budgetYears).reverse();
+    };
 
-	const handleChange = (event) => {
-		setBudgetYear(event.target.value);
-	};
-	const goToBudget = () => {
-		const year = budgetYear.substring(0, 4)
-		navigate(`/budget/${year}`);
-	};
+    const handleChange = (event) => {
+        setBudgetYear(event.target.value);
+    };
+    const goToBudget = () => {
+        const year = budgetYear.substring(0, 4)
+        navigate(`/budget/${year}`);
+    };
 
 
-	return (<><Box className={classes.box}>
-			<div className={classes.innerBox}>
-				<div className={classes.text}>
-					<DropDown list={getBudgetYears(allBudget)} value={budgetYear} onSelect={handleChange}/>
-				</div>
-			</div>
-			<div className={classes.actionButtons}>
-				<ActionButton label={"Open budget"} id={"addNewBudgetButton"} onClick={goToBudget}/>
-			</div>
+    return (<><Box className={classes.box}>
+            <div className={classes.innerBox}>
+                <div className={classes.text}>
+                    <DropDown list={getBudgetYears(allBudget)} value={budgetYear} onSelect={handleChange}/>
+                </div>
+            </div>
+            <div className={classes.actionButtons}>
+                <ActionButton label={"Open budget"} id={"addNewBudgetButton"} onClick={goToBudget}/>
+            </div>
 
-		</Box>
-			<div className={classes.box}>
-				<DataTable headings={budgetSummaryData(allBudget, budgetYear).headings}
-				           rows={budgetSummaryData(allBudget, budgetYear).data}
-				           title={`Budget Summary FY ${budgetYear} (in lakhs)`}/>
-				<Paper style={{
-					height: 400, width: '70%', paddingBottom: 20, paddingTop: 15, color: '#616161'
-				}}><ResponsivePieChart
-					data={budgetSummaryData(allBudget, budgetYear).pieChartData}
-					title={`Key Budget Highlights FY ${budgetYear} (in lakhs)`}
-				/></Paper>
-				<Paper style={{
-					height: 400,
-					width: '40%',
-					paddingBottom: 20,
-					paddingTop: 15,
-					color: '#616161',
-					paddingRight: 5,
-					paddingLeft: 5,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "space-around"
+        </Box>
+            <div className={classes.box}>
+                <DataTable headings={budgetSummaryData(allBudget, budgetYear).headings}
+                           rows={budgetSummaryData(allBudget, budgetYear).data}
+                           title={`Budget Summary FY ${budgetYear} (in lakhs)`}/>
+                <Paper style={{
+                    height: 400, width: '70%', paddingBottom: 20, paddingTop: 15, color: '#616161'
+                }}><ResponsivePieChart
+                    data={budgetSummaryData(allBudget, budgetYear).pieChartData}
+                    title={`Key Budget Highlights FY ${budgetYear} (in lakhs)`}
+                /></Paper>
+                <Paper style={{
+                    height: 400,
+                    width: '40%',
+                    paddingBottom: 20,
+                    paddingTop: 15,
+                    color: '#616161',
+                    paddingRight: 5,
+                    paddingLeft: 5,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-around"
 
-				}}>
-					<PerPersonExpenditure allBudget={allBudget} budgetYear={budgetYear}
-                                          municipalityPopulation={currentMunicipality ? currentMunicipality.population : 0}/>
-				</Paper>
-			</div>
-			<div className={classes.box}>
-				<Paper style={{
-					height: 400, width: '50%'
-				}}><ResponsiveBarGraph data={budgetSummaryData(allBudget, budgetYear).barGraphData} indexBy={"name"}
-				                       keys={['Revenue Income', 'Revenue Expenditure']}/>
-				</Paper>
-				<Paper style={{
-					height: 400,
-					width: '40%',
-					color: '#616161',
-					paddingRight: 5,
-					paddingLeft: 5,
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "space-around"
+                }}>
+                    <PerPersonExpenditure allBudget={allBudget} budgetYear={budgetYear}
+                                          municipalityPopulation={budgetSummaryData(allBudget, budgetYear).population}/>
+                </Paper>
+            </div>
+            <div className={classes.box}>
+                <Paper style={{
+                    height: 400, width: '50%'
+                }}><ResponsiveBarGraph data={budgetSummaryData(allBudget, budgetYear).barGraphData} indexBy={"name"}
+                                       keys={['Revenue Income', 'Revenue Expenditure']}/>
+                </Paper>
+                <Paper style={{
+                    height: 400,
+                    width: '40%',
+                    color: '#616161',
+                    paddingRight: 5,
+                    paddingLeft: 5,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-around"
 
-				}}>
-					<PerPersonRevenue allBudget={allBudget} budgetYear={budgetYear}
-                                      municipalityPopulation={currentMunicipality ? currentMunicipality.population : 0}/>
-				</Paper>
-			</div>
+                }}>
+                    <PerPersonRevenue allBudget={allBudget} budgetYear={budgetYear}
+                                      municipalityPopulation={budgetSummaryData(allBudget, budgetYear).population}/>
+                </Paper>
+            </div>
 
-		</>
+        </>
 
-	);
+    );
 };
 export default CurrentBudgetBox;
--- src/domain/budgetSummaryMapper.js
+++ src/domain/budgetSummaryMapper.js
@@ -3,120 +3,124 @@ import {fromContract} from "./budgetContractMapper";
 
 
 const budgetSummaryData = (budgets, year) => {
-	const currentYearBudget = _.chain(budgets)
-		.filter((e) => e.budgetYear === year)
-		.first()
-		.value()
-	const prevYearBudget = _.chain(budgets)
-		.filter((e) => e.budgetYear === `${year.substring(0, 4) - 1}-${year.substring(7, 5) - 1}`)
-		.first()
-		.value()
+    const currentYearBudget = _.chain(budgets)
+        .filter((e) => e.budgetYear === year)
+        .first()
+        .value()
+    const prevYearBudget = _.chain(budgets)
+        .filter((e) => e.budgetYear === `${year.substring(0, 4) - 1}-${year.substring(7, 5) - 1}`)
+        .first()
+        .value()
+    const getBudgetedValue = (budget, itemName) => {
+        let revenue = 0;
+        if (!_.isEqual(budget, undefined)) {
+            const revenueIncome = _.chain(fromContract(budget).items)
+                .filter((e) => e.majorHeadGroup === itemName)
+                .first()
+                .value()
+            revenue = revenueIncome.summary.budgetedAmount;
+        }
+        return revenue;
+    }
 
-	const getBudgetedValue = (budget, itemName) => {
-		let revenue = 0;
-		if (!_.isEqual(budget, undefined)) {
-			const revenueIncome = _.chain(fromContract(budget).items)
-				.filter((e) => e.majorHeadGroup === itemName)
-				.first()
-				.value()
-			revenue = revenueIncome.summary.budgetedAmount;
-		}
-		return revenue;
-	}
-
-	const getRevisedValue = (budget, itemName) => {
-		let revenue = 0;
-		if (!_.isEqual(budget, undefined)) {
-			const revenueIncome = _.chain(fromContract(budget).items)
-				.filter((e) => e.majorHeadGroup === itemName)
-				.first()
-				.value()
-			revenue = _.ceil(revenueIncome.summary.currentYear4MonthsProbables + revenueIncome.summary.currentYear8MonthsActuals);
-		}
-		return revenue;
-	}
-
-
-	const getHeadings = () => {
-		let headings = [];
-		headings.push('');
-		headings.push(`FY ${year.substring(0, 4) - 1} - ${year.substring(7, 5) - 1} (Revised Estimates)`);
-		headings.push(`FY ${year} (Budgeted Estimates)`);
-		return headings
-	};
-
-	const getBudgetedTotalSurplusOrDef = () => {
-		return (getBudgetedValue(currentYearBudget, 'Revenue Receipt') + getBudgetedValue(currentYearBudget, 'Assets')) - (getBudgetedValue(currentYearBudget, 'Expenses') + getBudgetedValue(currentYearBudget, 'Liability'))
-	}
-	const getRevisedTotalSurplusOrDef = () => {
-		return (getRevisedValue(prevYearBudget, 'Revenue Receipt') + getRevisedValue(prevYearBudget, 'Assets')) - (getRevisedValue(prevYearBudget, 'Expenses') + getRevisedValue(prevYearBudget, 'Liability'))
-	}
-
-	const getData = () => {
-		let dataLine = [];
-		dataLine.push({
-			name: 'Revenue Income',
-			revised: _.ceil(getRevisedValue(prevYearBudget, 'Revenue Receipt') / 100000),
-			budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Revenue Receipt') / 100000)
-		});
-		dataLine.push({
-			name: 'Revenue Expenditure',
-			revised: _.ceil(getRevisedValue(prevYearBudget, 'Expenses') / 100000),
-			budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Expenses') / 100000)
-		});
-		dataLine.push({
-			name: 'Capital Income',
-			revised: _.ceil(getRevisedValue(prevYearBudget, 'Assets') / 100000),
-			budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Assets') / 100000)
-		});
-		dataLine.push({
-			name: 'Capital Expenditure',
-			revised: _.ceil(getRevisedValue(prevYearBudget, 'Liability') / 100000),
-			budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Liability') / 100000)
-		});
-		dataLine.push({
-			name: 'Total surplus/deficit',
-			revised: _.ceil(getRevisedTotalSurplusOrDef(prevYearBudget, 'Liability') / 100000),
-			budgeted: _.ceil(getBudgetedTotalSurplusOrDef(currentYearBudget, 'Liability') / 100000)
-		});
-		return dataLine;
-	}
-	const piechartData = () => {
-		let pieData = [];
-		pieData.push({
-			id: 'Revenue Budget',
-			value: _.ceil(getBudgetedValue(currentYearBudget, 'Expenses') / 100000)
-		});
-		pieData.push({
-			id: 'Capital Budget',
-			value: _.ceil(getBudgetedValue(currentYearBudget, 'Liability') / 100000)
-		});
-		return pieData;
-	}
-	const getBarGraphData = () => {
-		let barData = [];
-		barData.push({
-			"name": 'Revenue Income',
-			"Revenue Income": _.ceil(getBudgetedValue(currentYearBudget, 'Revenue Receipt') / 100000)
-		});
-		barData.push({
-			"name": 'Revenue Expenditure',
-			"Revenue Expenditure": _.ceil(getBudgetedValue(currentYearBudget, 'Expenses') / 100000)
-		})
-		return barData;
-	}
+    const getRevisedValue = (budget, itemName) => {
+        let revenue = 0;
+        if (!_.isEqual(budget, undefined)) {
+            const revenueIncome = _.chain(fromContract(budget).items)
+                .filter((e) => e.majorHeadGroup === itemName)
+                .first()
+                .value()
+            revenue = _.ceil(revenueIncome.summary.currentYear4MonthsProbables + revenueIncome.summary.currentYear8MonthsActuals);
+        }
+        return revenue;
+    }
 
 
-	return {
-		headings: getHeadings(),
-		data: getData(),
-		pieChartData: piechartData(),
-		barGraphData: getBarGraphData(),
-		budgetedRevenueIncome:getBudgetedValue(currentYearBudget,'Revenue Receipt'),
-		budgetedRevenueExpenditure: getBudgetedValue(currentYearBudget, 'Expenses'),
-		budgetedCapitalExpenditure: getBudgetedValue(currentYearBudget, 'Liability')
+    const getHeadings = () => {
+        let headings = [];
+        headings.push('');
+        headings.push(`FY ${year.substring(0, 4) - 1} - ${year.substring(7, 5) - 1} (Revised Estimates)`);
+        headings.push(`FY ${year} (Budgeted Estimates)`);
+        return headings
+    };
 
-	};
+    const getBudgetedTotalSurplusOrDef = () => {
+        return (getBudgetedValue(currentYearBudget, 'Revenue Receipt') + getBudgetedValue(currentYearBudget, 'Assets')) - (getBudgetedValue(currentYearBudget, 'Expenses') + getBudgetedValue(currentYearBudget, 'Liability'))
+    }
+    const getRevisedTotalSurplusOrDef = () => {
+        return (getRevisedValue(prevYearBudget, 'Revenue Receipt') + getRevisedValue(prevYearBudget, 'Assets')) - (getRevisedValue(prevYearBudget, 'Expenses') + getRevisedValue(prevYearBudget, 'Liability'))
+    }
+
+    const getData = () => {
+        let dataLine = [];
+        dataLine.push({
+            name: 'Revenue Income',
+            revised: _.ceil(getRevisedValue(prevYearBudget, 'Revenue Receipt') / 100000),
+            budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Revenue Receipt') / 100000)
+        });
+        dataLine.push({
+            name: 'Revenue Expenditure',
+            revised: _.ceil(getRevisedValue(prevYearBudget, 'Expenses') / 100000),
+            budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Expenses') / 100000)
+        });
+        dataLine.push({
+            name: 'Capital Income',
+            revised: _.ceil(getRevisedValue(prevYearBudget, 'Assets') / 100000),
+            budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Assets') / 100000)
+        });
+        dataLine.push({
+            name: 'Capital Expenditure',
+            revised: _.ceil(getRevisedValue(prevYearBudget, 'Liability') / 100000),
+            budgeted: _.ceil(getBudgetedValue(currentYearBudget, 'Liability') / 100000)
+        });
+        dataLine.push({
+            name: 'Total surplus/deficit',
+            revised: _.ceil(getRevisedTotalSurplusOrDef(prevYearBudget, 'Liability') / 100000),
+            budgeted: _.ceil(getBudgetedTotalSurplusOrDef(currentYearBudget, 'Liability') / 100000)
+        });
+        return dataLine;
+    }
+    const piechartData = () => {
+        let pieData = [];
+        pieData.push({
+            id: 'Revenue Budget',
+            value: _.ceil(getBudgetedValue(currentYearBudget, 'Expenses') / 100000)
+        });
+        pieData.push({
+            id: 'Capital Budget',
+            value: _.ceil(getBudgetedValue(currentYearBudget, 'Liability') / 100000)
+        });
+        return pieData;
+    }
+    const getBarGraphData = () => {
+        let barData = [];
+        barData.push({
+            "name": 'Revenue Income',
+            "Revenue Income": _.ceil(getBudgetedValue(currentYearBudget, 'Revenue Receipt') / 100000)
+        });
+        barData.push({
+            "name": 'Revenue Expenditure',
+            "Revenue Expenditure": _.ceil(getBudgetedValue(currentYearBudget, 'Expenses') / 100000)
+        })
+        return barData;
+    }
+    const getPopulation = () => {
+        if (currentYearBudget) {
+            return currentYearBudget.population;
+        }
+        return 0;
+    }
+    return {
+        headings: getHeadings(),
+        data: getData(),
+        pieChartData: piechartData(),
+        barGraphData: getBarGraphData(),
+        budgetedRevenueIncome: getBudgetedValue(currentYearBudget, 'Revenue Receipt'),
+        budgetedRevenueExpenditure: getBudgetedValue(currentYearBudget, 'Expenses'),
+        budgetedCapitalExpenditure: getBudgetedValue(currentYearBudget, 'Liability'),
+        population: getPopulation()
+
+    };
 }
 
 
