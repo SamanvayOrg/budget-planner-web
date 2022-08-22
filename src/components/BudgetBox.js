20e86127fadb361c4bb42935deea6a5cf045317e
Sachin Kadam <Sachinkadamsk01@gmail.com>
2022-08-22 08:49:33 +0530

    add budget status on the all budgte and user should able to change budget status from all budget

--- src/components/BudgetBox.js
+++ src/components/BudgetBox.js
@@ -2,7 +2,10 @@ import Box from "@mui/material/Box";
 import {makeStyles} from "@mui/styles";
 import ActionButton from "./ActionButton";
 import {useTranslation} from "react-i18next";
-import React from "react";
+import React, {useState} from "react";
+import {Modal} from "@mui/material";
+import {useStyles} from "./ModalWithButton";
+import DropDown from "./DropDown";
 
 const styleSheets = makeStyles(theme => ({
     box: {
@@ -21,8 +24,6 @@ const styleSheets = makeStyles(theme => ({
         fontSize: "15px",
         fontWeight: "400",
         fontFamily: "Lato",
-
-
     }, insideBox: {
         display: "flex",
         flexDirection: "column",
@@ -33,7 +34,6 @@ const styleSheets = makeStyles(theme => ({
         marginLeft: "1%",
     }, lastUpdateText: {
         display: "flex",
-
         fontSize: "11px",
         textTransform: "uppercase",
         color: "#616161",
@@ -44,14 +44,61 @@ const styleSheets = makeStyles(theme => ({
         textTransform: "uppercase",
         color: "#616161",
         marginRight: "1%",
+    }, modal: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        fontFamily: "Lato",
+        fontStyle: "normal",
+    }, cancelText: {
+        cursor: "pointer",
+        display: "flex",
+        justifyContent: "center",
+        fontFamily: 'Lato',
+        fontStyle: "normal",
+        fontWeight: "700",
+        fontSize: "12px",
+        color: "#212121"
+    }, modalDiv: {
+        position: 'absolute',
+        display: "flex",
+        flexDirection: "column",
+        top: '30%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: "20%",
+        background: "whitesmoke",
+        border: '0px  #000',
+        boxShadow: "0px 0px 4px  #000000",
+        p: 4,
+        gap: "1vw",
+        padding: "25px 25px 25px 25px"
     }
 
 }))
-const BudgetBox = ({versionName, lastUpdated, index, firstButtonAction, secondButtonAction}) => {
+const BudgetBox = ({
+                       versionName,
+                       lastUpdated,
+                       index,
+                       firstButtonAction,
+                       secondButtonAction,
+                   }) => {
     const {t} = useTranslation();
     const classes = styleSheets();
+    const modalClass = useStyles();
+    const [budgetAllStatus, setBudgetAllStatus] = useState(['Draft', 'Approved by GBM', 'Approved by district']);
+    const [currentStatus, setCurrentStatus] = useState('Draft');
+    const [changedStatus, setChangedStatus] = useState(currentStatus);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-    
+    const onStateChange = () => {
+        setCurrentStatus(changedStatus);
+        handleClose();
+    }
+
+
     return (
         <Box className={classes.box} key={index}>
 			<span className={classes.insideBox}>
@@ -59,13 +106,29 @@ const BudgetBox = ({versionName, lastUpdated, index, firstButtonAction, secondBu
 					{lastUpdated}
 				</div>
 
-				{versionName}
+                {versionName}
+
+                <div>
+                <ActionButton label={t(currentStatus)} variant="outlined" onClick={handleOpen}/>
+                <Modal open={open} onClose={handleClose} className={modalClass.modal}>
+                        <div className={classes.modalDiv}>
+                            <DropDown list={budgetAllStatus} value={changedStatus}
+                                      onSelect={(e) => setChangedStatus(e.target.value)}/>
+                            <ActionButton label={t("Save")} id={"addNewBudgetButton"} onClick={onStateChange}/>
+                            <span className={classes.cancelText} onClick={handleClose}>Cancel</span>
+                        </div>
+                    </Modal>
+            </div>
+
+
 			</span>
             <span className={classes.actionButtons}>
-				<ActionButton style={{marginLeft: '10px'}} key={index.key} onClick={firstButtonAction}  label={t("DOWNLOAD")}
+				<ActionButton style={{marginLeft: '10px'}} key={index.key} onClick={firstButtonAction}
+                              label={t("DOWNLOAD")}
                               id={"smallActionButton"}/>
 
-				<ActionButton style={{marginLeft: '10px'}} key={index} onClick={secondButtonAction} label={t("EDIT BUDGET")}
+				<ActionButton style={{marginLeft: '10px'}} key={index} onClick={secondButtonAction}
+                              label={t("EDIT BUDGET")}
                               id={"smallActionButton"}/>
 			</span>
         </Box>
