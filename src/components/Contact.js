873780988fd09710a46e5f2af6dc8f6859b1f212
Arpita Roy <arpitaroysjpr@gmail.com>
2025-04-07 21:37:35 +0530

    Made contact functionality working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,32 @@
 import Typography from "@mui/material/Typography";
+import {style} from "./EmptyBudgetBox";
+import CustomModal from "./CustomModal";
+import {useTranslation} from "react-i18next";
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles(theme => ({
+    contact: {
+        cursor: "pointer",
+        color: "#000000",
+        '&:hover': {
+            textDecoration: "none"
+        }
+    }
+}));
 
 const Contact = () => {
-
-return(
-	<Typography id="navRightBarMenu">
-		Contact
-	</Typography>
-)
+    const {t} = useTranslation();
+    const classes = useStyles();
+    return (
+        <Typography id="contact" className={classes.contact}>
+            <CustomModal
+                isButton={false}
+                buttonLabel={t("Contact")}
+                modalText={t("Please connect with us at: citybudgets.support@cept.ac.in")}
+                style={style}
+            />
+        </Typography>
+    )
 }
+
 export default Contact;
